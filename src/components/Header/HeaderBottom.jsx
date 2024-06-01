a02eb131511e2276197d9a420cf369ac03055a57
IKANYI <paulkaranja214@gmail.com>
2024-06-01 19:43:19 +0300

    added navigation-menu

--- src/components/Header/HeaderBottom.jsx
+++ src/components/Header/HeaderBottom.jsx
@@ -1,7 +1,14 @@
 const HeaderBottom = () => {
   return(
     <div className="headerBottom">
-
+      <nav className="header-nav">
+        <ol className="header-nav-list">
+        <li className="header-nav-item"><a href="">Home</a></li>
+        <li className="header-nav-item"><a href="">Leadership</a></li>
+        <li className="header-nav-item"><a href="">tracks</a></li>
+        <li className="header-nav-item"><a href="">Event</a></li>
+        </ol>
+      </nav>
     </div>
   )
 }
