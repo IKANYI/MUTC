eb493bf80bb64357bd801a8b96d24633b116bad9
IKANYI <paulkaranja214@gmail.com>
2024-06-03 06:51:37 +0300

    lay the structure of tracks and events

--- src/pages/Event/Event.jsx
+++ src/pages/Event/Event.jsx
@@ -1,8 +1,25 @@
 const Event = () => {
   return(
-    <h2>
-      Event
-    </h2>
+  <section className="event-container">
+    <div className="event-desc">
+      <p>Cyber security - every Monday from 4pm to 7pm at computer lab F04, led by Webster Ifedha.</p>
+    </div>
+    <div className="event-desc">
+      <p>UI/UX - every Tuesday from 5pm to 8pm at computer lab F04, led by Manase Gunga.</p>
+    </div>
+    <div className="event-desc">
+      <p>Web development - every Wednesday from 6pm to 9pm at computer lab F04, led by Carolyne Githenduka</p>
+    </div>
+    <div className="event-desc">
+      <p>Mobile apps development - every Thursday from 4pm to 7pm led by Stanley Amunze.</p>
+    </div>
+    <div className="event-desc">
+      <p>Cloud engineering - every Friday from 3pm to 6pm at computer lab F04, led by Paul Karanja.</p>
+    </div>
+    <div className="event-desc">
+      <p>Power Platform - every Saturday from 1pm to 4pm at computer lab F04, led by Evyonn Mbithe.</p>
+    </div>
+  </section>
   )
 }
 export default Event;
