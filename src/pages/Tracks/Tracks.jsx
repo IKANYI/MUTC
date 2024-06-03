413340a31fca2d37847aa675c8ed2559692a0595
IKANYI <paulkaranja214@gmail.com>
2024-06-03 08:12:26 +0300

    add form in track

--- src/pages/Tracks/Tracks.jsx
+++ src/pages/Tracks/Tracks.jsx
@@ -20,6 +20,28 @@ const Tracks = () => {
     <div className="track-items">
       <p>Power Platform - Students who select the Power Platform track will explore the capabilities of Microsoft's Power Platform, which includes Power BI, Power Apps, Power Automate, and Power Virtual Agents. They will learn how to create custom business applications, automate workflows, analyze data, and build chatbots with minimal coding. Through hands-on projects and interactive workshops, students will gain practical experience in leveraging these tools to enhance business processes and drive data-driven decision-making. By the end of the track, students will be proficient in developing integrated solutions using the Power Platform, enabling them to transform organizational operations and improve efficiency.</p>
     </div>
+    <form action="" className="student-info">
+      <label htmlFor="fname">First Name:</label>
+      <input type="text"id="fname" placeholder="Paul" required />
+      <label htmlFor="lname">Last Name:</label>
+      <input type="text" id="lname" placeholder="Walker" required />
+      <label htmlFor="email">Email Address:</label>
+      <input type="text" id="email" placeholder="Email" required />
+      <label htmlFor="corse">Course of Study:</label>
+      <input type="text" id="course" placeholder="Software Engineering" required />
+      <label htmlFor="yos">Year of Study:</label>
+      <input type="text" id="yos" placeholder="One"  required />
+      <label htmlFor="track">Desired Track:</label>
+      <select name="track" id="track">
+      <option value="volvo">Cyber Security</option>
+      <option value="saab">UX/UI design</option>
+      <option value="mercedes">Web Developmen</option>
+      <option value="audi">Mobile App Development</option>
+      <option value="audi">Cloud Engineering</option>
+      <option value="audi">Power Platform</option>
+      </select>
+      <buttton className="button" type= "submit">Submit</buttton>
+    </form>
   </section>
   )
 }
